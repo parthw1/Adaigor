91a3cd18c9eb3cc7bb942453d09dfb8da9d8e6e8
parth <parthwazurkar@gmail.com>
2017-12-13 00:00:39 +0530

    Database initialsed and conected

--- music/urls.py
+++ music/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # /music/
     url(r'^$', views.index, name='index'),
+
+    # /music/id_number
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
-
+from .models import Album
 # Create your views here.
 #################
 
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>This is the music app homepage</h1>")
+    all_albums = Album.objects.all()
+    html = ''
+    for album in all_albums:
+        url = '/music/' + str(album.id) + '/'
+        html += '<a href="' + url + '">' + album.album_title + '</a><br>'
+    return HttpResponse(html)
+
+def detail(request, album_id):
+    return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
