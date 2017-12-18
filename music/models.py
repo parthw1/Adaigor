a3913ce75c4841996d75344761c20c319f5503ae
parth <parthwazurkar@gmail.com>
2017-12-18 21:28:16 +0530

    Update

--- music/models.py
+++ music/models.py
@@ -8,10 +8,13 @@ class Album(models.Model):
     genre = models.CharField(max_length=100)
     album_logo = models.CharField(max_length=1000)
     def __str__(self):
-        return self.album_title + ' ' + self.artist
+        return self.album_title + ' - ' + self.artist
 
 
 class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     file_type = models.CharField(max_length=10)
     song_title = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.song_title
--- music/views.py
+++ music/views.py
@@ -1,4 +1,6 @@
+from django.http import Http404
 from django.shortcuts import render
+#from django.template import loader
 from .models import Album
 # Create your views here.
 #################
@@ -7,11 +9,15 @@ from django.http import HttpResponse
 
 def index(request):
     all_albums = Album.objects.all()
-    html = ''
-    for album in all_albums:
-        url = '/music/' + str(album.id) + '/'
-        html += '<a href="' + url + '">' + album.album_title + '</a><br>'
-    return HttpResponse(html)
+    #template =  loader.get_template('music/index.html')
+    context = {'all_albums' :all_albums}
+    #return HttpResponse(template.render(context, request))
+    return render(request, 'music/index.html', context)
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
+    #return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist")
+    return render(request, 'music/details.html', {'album' :album})
