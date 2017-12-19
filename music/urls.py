bebe8913f5ae56b8d5425897758461f2f8e95a2b
parth <parthwazurkar@gmail.com>
2017-12-19 13:02:54 +0530

    Updtae

--- music/urls.py
+++ music/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'music'
+
 urlpatterns = [
     # /music/
     url(r'^$', views.index, name='index'),
--- music/views.py
+++ music/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_obect_or_404
 #from django.template import loader
 from .models import Album
 # Create your views here.
@@ -16,8 +16,11 @@ def index(request):
 
 def detail(request, album_id):
     #return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
+    album = get_obect_or_404(Album, pk=album_id)
+    '''
     try:
         album = Album.objects.get(pk=album_id)
     except Album.DoesNotExist:
         raise Http404("Album does not exist")
+    '''
     return render(request, 'music/details.html', {'album' :album})
