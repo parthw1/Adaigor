b456905403a73226e9bc06e58121691885d19805
parth <parthwazurkar@gmail.com>
2017-12-27 17:21:31 +0530

    Added name

--- music/views.py
+++ music/views.py
@@ -1,7 +1,12 @@
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+#from django.views.generic import generic
+from django.views.generic import View
 from .models import Album
+from .forms import UserForm
 
 class IndexView(generic.ListView):
     template_name = 'music/index.html'
@@ -25,3 +30,37 @@ class AlbumUpdate(UpdateView):
 class AlbumDelete(DeleteView):
     model = Album
     success_url = reverse_lazy('music:index')
+
+
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'music/registration_form.html'
+
+    #display blank forms
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form':form})
+
+    #process form data
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+
+            #cleaned (normalized) data
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+            #returns User obects if credentials are correct
+            user = authenticate(usename=username , password=password)
+
+            if user is not None:
+
+                if user.is_active:
+                    login(request, user)
+                    return redirect('music:index')
+
+        return render(request, self.template_name, {'form':form})
