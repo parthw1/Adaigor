cf57c1abd24be5c9cf72a3124d678a018c5eda2c
parth <parthwazurkar@gmail.com>
2017-12-27 13:03:39 +0530

    Added generic views

--- music/urls.py
+++ music/urls.py
@@ -5,11 +5,9 @@ app_name = 'music'
 
 urlpatterns = [
     # /music/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
 
     # /music/id_number
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
-    # /music/id_number/favorite
-    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,40 +1,13 @@
-from django.http import Http404
-from django.shortcuts import render
-#from django.template import loader
-from .models import Album, Song
-# Create your views here.
-#################
+from django.views import generic
+from .models import Album
 
-from django.http import HttpResponse
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
+    context_object_name = 'all_albums'
+    def get_queryset(self):
+        return Album.objects.all()
 
-def index(request):
-    all_albums = Album.objects.all()
-    #template =  loader.get_template('music/index.html')
-    context = {'all_albums' :all_albums}
-    #return HttpResponse(template.render(context, request))
-    return render(request, 'music/index.html', context)
 
-def detail(request, album_id):
-    #return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
-    #album = get_obect_or_404(Album, pk=album_id)
-
-    try:
-        album = Album.objects.get(pk=album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album does not exist")
-    return render(request, 'music/details.html', {'album' :album})
-
-def favorite(request, album_id):
-    try:
-        album = Album.objects.get(pk=album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album does not exist")
-    try:
-        selected_song = album.song_set.get(pk=request.POST['song'])
-    except(KeyError, Song.DoesNotExist):
-        return render(request, 'music/details.html', {'album' :album,
-        'error_message': "You did not selet a valid song",})
-    else:
-        selected_song.is_favorite = True
-        selected_song.save()
-        return render(request, 'music/details.html', {'album' :album})
+class DetailView(generic.DetailView):
+    model = Album
+    template_name = 'music/details.html'
